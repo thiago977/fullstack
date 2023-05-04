a366d9bfe95d1c8db76f651e27b5aedca96d7480
Thiago de Lima <thiagodelima1997@gmail.com>
2023-05-04 09:51:23 -0300

    Ex 1 JS

---
+++ OneBitCode/JS_1_Logica/ex1.js
@@ -0,0 +1,35 @@
+// let nome = prompt("Qual o seu primeiro nome?");
+// let sobreNome = prompt("Qual o seu sobrenome?");
+
+// let estudo = prompt("Qual o seu campo de estudo?");
+// let nascimento = prompt("Qual ano de nascimento?");
+
+// alert(
+//   "Recruta: " + nome + " " + sobreNome +
+//   "\nÁrea: " + estudo +
+//   "\nIdade: " 2023 - nascimento );
+
+// Calculadora
+
+const varA = prompt("Insira o primeiro número");
+
+const varB = prompt("Insira o segundo número");
+
+const x = parseFloat(varA);
+const y = parseFloat(varB);
+
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+
+alert(
+  "Soma: " +
+    soma +
+    "\nSubração: " +
+    subtracao +
+    "\nMultiplicação: " +
+    multiplicacao +
+    "\nDivisão: " +
+    divisao
+);
